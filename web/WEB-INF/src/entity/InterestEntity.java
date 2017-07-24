af02567822281d8c0b15f2ed9ff63e5df7409245
Inoria <s842642463@163.com>
2017-07-24 09:41:29 +0800

    7.24 改了interset的hfg配置文件

--- web/WEB-INF/src/entity/InterestEntity.java
+++ web/WEB-INF/src/entity/InterestEntity.java
@@ -64,31 +64,4 @@ public class InterestEntity {
         this.other = other;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        InterestEntity that = (InterestEntity) o;
-
-        if (userid != null ? !userid.equals(that.userid) : that.userid != null) return false;
-        if (music != null ? !music.equals(that.music) : that.music != null) return false;
-        if (sport != null ? !sport.equals(that.sport) : that.sport != null) return false;
-        if (book != null ? !book.equals(that.book) : that.book != null) return false;
-        if (movie != null ? !movie.equals(that.movie) : that.movie != null) return false;
-        if (game != null ? !game.equals(that.game) : that.game != null) return false;
-        return other != null ? other.equals(that.other) : that.other == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = userid != null ? userid.hashCode() : 0;
-        result = 31 * result + (music != null ? music.hashCode() : 0);
-        result = 31 * result + (sport != null ? sport.hashCode() : 0);
-        result = 31 * result + (book != null ? book.hashCode() : 0);
-        result = 31 * result + (movie != null ? movie.hashCode() : 0);
-        result = 31 * result + (game != null ? game.hashCode() : 0);
-        result = 31 * result + (other != null ? other.hashCode() : 0);
-        return result;
-    }
 }
