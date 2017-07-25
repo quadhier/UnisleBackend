ce55dd3dd1771e1188a1ee4e5b9b2e561db27547
qdh <qudhier@qq.com>
2017-07-25 13:09:20 +0800

    self information edit finished, options and album unfinished

--- web/resources/js/relation/relation.js
+++ web/resources/js/relation/relation.js
@@ -11,24 +11,23 @@ $(document).ready(function () {
     });
 
     /*
-    *
-    * 函数定义
-    *
-    * */
+     *
+     * 函数定义
+     *
+     * */
     function strDisplay(str) {
-        if(str === null || str.length === 0) {
+        if (str === null || str.length === 0) {
             return "";
         }
         return str;
     }
 
 
-
     /*
-    *
-    * 初始化朋友信息
-    *
-    * */
+     *
+     * 初始化朋友信息
+     *
+     * */
     $.ajax({
         type: "GET",
         url: "friend/getFriend",
@@ -46,7 +45,7 @@ $(document).ready(function () {
         },
         success: function (res) {
 
-            if(res.result === "SUCCESS") {
+            if (res.result === "SUCCESS") {
                 $.each(res.data, function (index, user) {
 
                     var school = strDisplay(user.school);
@@ -54,17 +53,17 @@ $(document).ready(function () {
                     var grade = strDisplay(user.grade) === "" ? "" : strDisplay(user.grade) + "级";
                     var sex = user.sex;
                     var userHeadImg = user.userpic;
-                    if(userHeadImg === null || userHeadImg.length <= 0) {
-                        if(sex === "male") {
+                    if (userHeadImg === null || userHeadImg.length <= 0) {
+                        if (sex === "male") {
                             userHeadImg = "pic/userpic/male.jpg";
-                        } else if(sex === "female") {
+                        } else if (sex === "female") {
                             userHeadImg = "pic/userpic/female.jpeg";
                         } else {
                             userHeadImg = "pic/userpic/default.jpg"
                         }
                     }
 
-                    $(".myfriend_container .friendPanel").append("<div class='friend_container'>" +
+                    $(".myfriend_container .friendPanel").append("<div class='friend_container' data-userid='" + user.userid + "'>" +
                         "<div class='headimg'><img src='" + userHeadImg + "'width='105' height='70'></div>" +
                         "<div class='infoDiv'>" +
                         "<p class='pSchool'>" + user.nickname + "</p>" +
@@ -72,7 +71,7 @@ $(document).ready(function () {
                         "</div>" +
                         "</div>");
                 });
-            } else if(res.reason === "LOGINERROR") {
+            } else if (res.reason === "LOGINERROR") {
                 window.location.replace("message.html");
             } else {
                 alert(res.reason);
@@ -80,6 +79,89 @@ $(document).ready(function () {
         }
     });
 
+
+    /*
+     *
+     * 加载群组信息
+     *
+     * */
+
+    $.ajax({
+        type: "GET",
+        url: "group/useradded",
+        dataType: "json",
+        data: {
+            viewedUser: "10000000005"
+        },
+        timeout: 5000,
+        cache: false,
+        beforeSend: function () {
+
+        },
+        error: function () {
+            alert("检索群组网络请求失败!");
+        },
+        success: function (res) {
+
+            if(res.result === "SUCCESS" && res.data !== null) {
+                $.each(res.data, function (index, obj) {
+                    $(".findorg_container .container_body").prepend("<div class='small_friend_container rightgroup' data-groupid='" + obj.groupid + "' >"+
+                        "<div class='headimg'><img src='images/relation/headimg.png' width='105' height='70'></div>"+
+                        "<div class='infoDiv'>"+
+                        "<p class='pSchool'>"+obj.name+"</p>"+
+                        "<p class='pInfo'>"+obj.groupid+"</p>"+
+                        "</div>"+
+                        "</div>");
+                })
+            }
+        }
+    });
+
+
+    /*
+    *
+    * 加载可能认识的好友
+    *
+    * */
+
+    $.ajax({
+        type: "GET",
+        url: "friend/maybefriend",
+        dataType: "json",
+        data: {
+            viewedUser: "10000000002"
+        },
+        timeout: 5000,
+        cache: false,
+        beforeSend: function () {
+
+        },
+        error: function () {
+            alert("检索可能认识的好友网络请求失败!");
+        },
+        success: function (res) {
+
+            if(res.result === "SUCCESS" && res.data !== null) {
+
+                $.each(res.data, function(index, obj){
+
+                    var nickname = obj.nickname;
+                    var school = obj.school == null ? "" : obj.school;
+                    var department = obj.department == null ? "" : obj.department;
+                    var grade  = obj.grade == null ? "" : obj.grade;
+
+                    $(".findfriend_container .container_body").prepend("<div class='small_friend_container leftfriend' data-userid='" + obj.userid + "' >"+
+                        "<div class='headimg'><img src='images/relation/headimg.png' width='105' height='70'></div>"+
+                        "<div class='infoDiv'>"+
+                        "<p class='pSchool'>"+ nickname +"</p>"+
+                        "<p class='pInfo'>" + school + department +"<br>" + grade + "</p>"+
+                        "</div>"+
+                        "</div>");
+                });
+
+            }
+        }
+    });
 
 
     /*
@@ -163,10 +245,10 @@ $(document).ready(function () {
                                         },
                                         success: function (res) {
 
-                                            if(res.result === "SUCCESS") {
+                                            if (res.result === "SUCCESS") {
                                                 alert("发送好友请求成功");
                                             } else {
-                                                if(res.reason === "E_ALREADY_SENDED") {
+                                                if (res.reason === "E_ALREADY_SENDED") {
                                                     alert("您已发送过好友申请，请耐心等待对方回复")
                                                 } else if (res.reason === "E_SEND_ERROR") {
                                                     alert("好友申请发送失败");
@@ -177,9 +259,9 @@ $(document).ready(function () {
                                     }); // 发送好友申请结束
 
                                 } else {
-                                    if(res.reason === "E_FRIEND_ALREADY_ADDED") {
+                                    if (res.reason === "E_FRIEND_ALREADY_ADDED") {
                                         alert("对方已是您的好友");
-                                    } else if(res.reason === "E_YOU_ARE_IN_BLACKLIST") {
+                                    } else if (res.reason === "E_YOU_ARE_IN_BLACKLIST") {
                                         alert("您被加入黑名单，无法提出好友申请");
                                     }
                                 }
@@ -195,10 +277,10 @@ $(document).ready(function () {
 
 
     /*
-    *
-    * 为发送群组申请按钮绑定事件
-    *
-    * */
+     *
+     * 为发送群组申请按钮绑定事件
+     *
+     * */
 
     $("#findorg_btn").click(function () {
         var groupid = $("#findorg_input").val();
@@ -221,7 +303,7 @@ $(document).ready(function () {
                 alert("检索群组网络请求失败!");
             },
             success: function (res) {
-                if(res.result === "SUCCESS") {
+                if (res.result === "SUCCESS") {
                     //
                     // 发送申请
                     //
@@ -241,11 +323,11 @@ $(document).ready(function () {
                             alert("发送申请网络请求失败!");
                         },
                         success: function (res) {
-                            if(res.result === "SUCCESS") {
+                            if (res.result === "SUCCESS") {
                                 alert("群组加入申请发送成功");
-                            } else if(res.reason === "E_ALREADY_JOIN") {
+                            } else if (res.reason === "E_ALREADY_JOIN") {
                                 alert("您已经该群组的成员");
-                            } else if(res.reason === "E_ALREADY_SENDED") {
+                            } else if (res.reason === "E_ALREADY_SENDED") {
                                 alert("您已发送过群组申请，请耐心等候");
                             }
                         }
@@ -253,13 +335,41 @@ $(document).ready(function () {
                     });
 
                 } else {
-                    if(res.reason === "E_INVALID_GROUPID") {
+                    if (res.reason === "E_INVALID_GROUPID") {
                         alert("所寻找的组不存在");
                     }
                 }
             }
         });
     });
+
+    
+    /*
+    *
+    * 为点击好友绑定事件，跳转到好友的信息页面
+    *
+    * */
+    $(".leftfriend").live("click", function () {
+
+        alert($(this).data("userid"));
+    });
+
+    $(".friend_container").live("click", function () {
+        alert($(this).data("userid"));
+    });
+
+
+
+    /*
+    *
+    * 为点击群组绑定事件，跳转到群组的信息页面
+    *
+    * */
+    $(".rightgroup").live("click", function () {
+
+        alert($(this).data("groupid"));
+    })
+
 
 
 });
